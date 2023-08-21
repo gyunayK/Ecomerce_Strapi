452b4107579c1c7194d97ae43b87ff78f3ec5cca
OdinOfRivia <gyunay@hotmail.dk>
2023-08-21 13:25:31 -0700

    Fixed -Updated the webp format of the imgaes, the previus way was logging issues in the console

--- eComerce/client/src/components/Slider/Slider.jsx
+++ eComerce/client/src/components/Slider/Slider.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
@@ -7,17 +6,18 @@ import "./Slider.scss";
 
 const Slider = () => {
   const [current, setCurrent] = useState(0);
-
+  
   const prevSlide = () => {
     setCurrent(current === 0 ? 2 : current - 1);
   };
-
+  
   const nextSlide = () => {
     setCurrent(current === 2 ? 0 : current + 1);
   };
+  {/* <img src="/img/payment.png" alt="payment" /> */}
 
   const data = [
-    "https://ik.imagekit.io/riviaa/ImgEC/769525.jpg?updatedAt=1691194753862",
+    "/img/womanWithBag.webp",
     "https://ik.imagekit.io/riviaa/ImgEC/back-view-portrait-of-walking-stylish-african-american-man-wear-on-sunglasses-and-cap-with-handbag-outdoor-street-fashion-black-man-photo.jpg?updatedAt=1691194629793",
     "https://ik.imagekit.io/riviaa/ImgEC/istockphoto-627047390-612x612.jpg?updatedAt=1691194628331",
   ];
