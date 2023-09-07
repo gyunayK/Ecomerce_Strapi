af346eacb6204fe76ddd8e28c2c9e57651d84bf3
OdinOfRivia <gyunay@hotmail.dk>
2023-09-07 02:43:52 -0700

    style: improved suggestions component

--- eComerce/client/src/components/Suggestion/Suggestions.jsx
+++ eComerce/client/src/components/Suggestion/Suggestions.jsx
@@ -1,25 +1,18 @@
-import { useState, useEffect, useMemo } from "react";
 import "./Suggestions.scss";
+import { useState, useEffect, useMemo } from "react";
 import useFetch from "@/hooks/useFetch";
 import Card from "@/components/Card/Card";
-
 
 function Suggestions({ productID }) {
   const [subCategoryId, setSubCategoryId] = useState(null);
   const [suggestedProducts, setSuggestedProducts] = useState([]);
   const url = import.meta.env.VITE_APP_URL_API;
 
-  const {
-    data: productData,
-    loading: productLoading,
-    error: productError,
-  } = useFetch(`${url}/products?populate=*&[filters][id][$eq]=${productID}`);
+  const { data: productData } = useFetch(
+    `${url}/products?populate=*&[filters][id][$eq]=${productID}`
+  );
 
-  const {
-    data: suggestedData,
-    loading: suggestedLoading,
-    error: suggestedError,
-  } = useFetch(
+  const { data: suggestedData } = useFetch(
     subCategoryId
       ? `${url}/sub-categories?populate=products.img,products.img2&[filters][id][$eq]=${subCategoryId}`
       : null
@@ -28,7 +21,7 @@ function Suggestions({ productID }) {
   const filteredProducts = useMemo(() => {
     return suggestedProducts
       .filter((product) => product.id !== parseInt(productID))
-      .slice(0, 4); // This will limit the array to a maximum of 4 elements
+      .slice(0, 4); // maximum of 4 suggested products
   }, [suggestedProducts, productID]);
 
   useEffect(() => {
@@ -48,15 +41,18 @@ function Suggestions({ productID }) {
   }, [suggestedData]);
 
   return (
-    <div className="suggestionsWrapper">
-      <h1>You may also like</h1>
-
-      <div className="suggestions">
-        {filteredProducts.map(({ id, attributes }) => (
-          <Card key={id} item={attributes} id={id} />
-        ))}
-      </div>
-    </div>
+    <>
+      {filteredProducts.length > 0 && (
+        <div className="suggestionsWrapper">
+          <h1>You may also like</h1>
+          <div className="suggestions">
+            {filteredProducts.map(({ id, attributes }) => (
+              <Card key={id} item={attributes} id={id} />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
