d79b6adfceae70d3b927d055504de1e26d69739d
OdinOfRivia <gyunay@hotmail.dk>
2023-09-21 12:56:14 -0700

    feat: add orders from oder to cart

--- eComerce/client/src/components/Order/Order.jsx
+++ eComerce/client/src/components/Order/Order.jsx
@@ -1,143 +1,101 @@
 import { useState } from "react";
 import "./Order.scss";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { addToCart } from "@/redux/cartReducer";
 
 function Order({ user }) {
-  const [oders, setOders] = useState([...user.orders]);
+  const [orders, setOrders] = useState([...user.orders]);
+  const dispatch = useDispatch();
 
-  console.log(oders);
+  const url_IMG = import.meta.env.VITE_APP_UPLOAD_URL;
+
+  const handleCopyClick = (stripeId) => {
+    navigator.clipboard
+      .writeText(stripeId)
+      .then(() => {
+        toast.success("Copied!");
+      })
+      .catch(() => {
+        toast.error("Failed to copy!");
+      });
+  };
+
+  const orderTotalPrice = (products) => {
+    let totalPrice = 0;
+    products.forEach((product) => {
+      totalPrice += product.price;
+    });
+    return totalPrice.toFixed(2);
+  };
+
+  const handleAddToCart = (product) => {
+    console.log(product);
+    product.forEach((product) => {
+      dispatch(
+        addToCart({
+          id: product.id,
+          title: product.title,
+          desc: product.desc,
+          img: product.img,
+          price: product.price,
+          quantity: product.quantity,
+        })
+      );
+    });
+  };
+
+  console.log(orders);
 
   return (
     <div className="orderContainer">
-      <div className="oder">
-        <div className="oderTop">
-          <h1>Completed</h1>
-          <div className="oderTopRight">
-            <div>
-              <p>Oder Date: May 9, 2022</p>
-              <p>Oder ID: 23131231231232</p>
+      {orders.map((order, i) => {
+        return (
+          <div className="oder" key={i}>
+            <div className="oderTop">
+              <h1>Completed</h1>
+              <div className="oderTopRight">
+                <div>
+                  <p>Oder Date: {new Date(order.createdAt).toLocaleString()}</p>
+                  <p>
+                    Order ID: {order.stripeId.slice(0, 20)}...
+                    <a
+                      className="copyBTN"
+                      onClick={() => handleCopyClick(order.stripeId)}
+                    >
+                      Copy ID
+                    </a>
+                  </p>
+                </div>
+                <div>
+                  <a>Order details</a>
+                </div>
+              </div>
             </div>
-            <div>
-              <button>Order details</button>
-            </div>
-          </div>
-        </div>
-        <div className="oderBot">
-          <div className="imgContainer">
-            <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" />
-            <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" />
-            <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" />
-          </div>
-          <div className="orderBotRight">
-            <h2>Total: US 106.50</h2>
-            <button className="addToCartBTN">Add to cart</button>
-            <button className="deleteBTN"> Delete</button>
-          </div>
-        </div>
-      </div>
-      <div className="oder">
-        <div className="oderTop">
-          <h1>Completed</h1>
-          <div className="oderTopRight">
-            <div>
-              <p>Oder Date: May 9, 2022</p>
-              <p>Oder ID: 23131231231232</p>
-            </div>
-            <div>
-              <button>Order details</button>
-            </div>
-          </div>
-        </div>
-        <div className="oderBot">
-          <div className="imgContainer">
-            <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" />
-            <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" />
-            <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" />
-          </div>
-          <div className="orderBotRight">
-            <h2>Total: US 106.50</h2>
-            <button className="addToCartBTN">Add to cart</button>
-            <button className="deleteBTN"> Delete</button>
-          </div>
-        </div>
-      </div>
-      <div className="oder">
-        <div className="oderTop">
-          <h1>Completed</h1>
-          <div className="oderTopRight">
-            <div>
-              <p>Oder Date: May 9, 2022</p>
-              <p>Oder ID: 23131231231232</p>
-            </div>
-            <div>
-              <button>Order details</button>
+            <div className="oderBot">
+              <div className="imgContainer">
+                {order.products.map((product, i) => {
+                  return (
+                    <a key={i} href={`/product/${product.id}`}>
+                      <img src={`${url_IMG}${product.img}`} alt="" />
+                    </a>
+                  );
+                })}
+              </div>
+              <div className="orderBotRight">
+                <h2>Total: ${orderTotalPrice(order.products)}</h2>
+                <button
+                  className="addToCartBTN"
+                  onClick={() => handleAddToCart(order.products)}
+                >
+                  Add to cart
+                </button>
+                <button className="deleteBTN"> Delete</button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="oderBot">
-          <div className="imgContainer">
-            <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" />
-            <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" />
-            <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" />
-          </div>
-          <div className="orderBotRight">
-            <h2>Total: US 106.50</h2>
-            <button className="addToCartBTN">Add to cart</button>
-            <button className="deleteBTN"> Delete</button>
-          </div>
-        </div>
-      </div>
-      <div className="oder">
-        <div className="oderTop">
-          <h1>Completed</h1>
-          <div className="oderTopRight">
-            <div>
-              <p>Oder Date: May 9, 2022</p>
-              <p>Oder ID: 23131231231232</p>
-            </div>
-            <div>
-              <button>Order details</button>
-            </div>
-          </div>
-        </div>
-        <div className="oderBot">
-          <div className="imgContainer">
-            <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" />
-            <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" />
-            <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" />
-          </div>
-          <div className="orderBotRight">
-            <h2>Total: US 106.50</h2>
-            <button className="addToCartBTN">Add to cart</button>
-            <button className="deleteBTN"> Delete</button>
-          </div>
-        </div>
-      </div>
-      <div className="oder">
-        <div className="oderTop">
-          <h1>Completed</h1>
-          <div className="oderTopRight">
-            <div>
-              <p>Oder Date: May 9, 2022</p>
-              <p>Oder ID: 23131231231232</p>
-            </div>
-            <div>
-              <button>Order details</button>
-            </div>
-          </div>
-        </div>
-        <div className="oderBot">
-          <div className="imgContainer">
-            <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" />
-            <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" />
-            <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" />
-          </div>
-          <div className="orderBotRight">
-            <h2>Total: US 106.50</h2>
-            <button className="addToCartBTN">Add to cart</button>
-            <button className="deleteBTN"> Delete</button>
-          </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
