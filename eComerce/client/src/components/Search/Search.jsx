f895bb0d2ec10433aee3d9020f905f76555ef24e
OdinOfRivia <gyunay@hotmail.dk>
2023-10-21 23:48:58 -0700

    fix: optimizie search

--- eComerce/client/src/components/Search/Search.jsx
+++ eComerce/client/src/components/Search/Search.jsx
@@ -6,16 +6,20 @@ import useFetch from "@/hooks/useFetch";
 function Search() {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchItems, setSearchItems] = useState([]);
-  const searchRef = useRef(null);
-  const url = import.meta.env.VITE_APP_URL_API;
-  const sanitizedSearchTerm = searchTerm.trim().replace(/\s+/g, " ");
 
-  // Using encodeURIComponent to ensure special characters are handled properly
-  const fetchUrl = `${url}/products?populate=*&[filters][title][$contains]=${encodeURIComponent(
-    sanitizedSearchTerm
+  const searchRef = useRef(null);
+
+  const url = import.meta.env.VITE_APP_URL_API;
+
+  //every word has to be capitalized
+  const fetchUrl = `${url}/products?populate=*&[filters][title][$contains]=${searchTerm.replace(
+    /\b[a-z]/g,
+    (char) => char.toUpperCase()
   )}`;
 
-  const shouldFetch = sanitizedSearchTerm !== "";
+  console.log(fetchUrl);
+
+  const shouldFetch = searchTerm !== "";
   const { data } = useFetch(fetchUrl, shouldFetch);
 
   const handleOutsideClick = (e) => {
@@ -30,6 +34,10 @@ function Search() {
       setSearchTerm("");
       setSearchItems([]);
     }, 500);
+
+    return () => {
+      clearTimeout();
+    };
   };
 
   useEffect(() => {
@@ -41,7 +49,7 @@ function Search() {
   }, []);
 
   useEffect(() => {
-    if (sanitizedSearchTerm === "") {
+    if (searchTerm === "") {
       setSearchItems([]);
       return;
     }
@@ -49,7 +57,7 @@ function Search() {
     if (data) {
       setSearchItems(data);
     }
-  }, [data, sanitizedSearchTerm]);
+  }, [data, searchTerm]);
 
   return (
     <div className="search-box" ref={searchRef}>
@@ -64,31 +72,33 @@ function Search() {
         value={searchTerm}
         onBlur={handleBlur}
       />
-      {searchItems.length > 0 && (
+      {searchItems.length !== 0 ? (
         <div className="searchItems">
-          {searchItems.map((item) => (
-            <div className="searchItem" key={item.id}>
-              <Link
-                className="searchLink"
-                to={`/product/${item.attributes.title}`}
-                onClick={() => {
-                  setSearchTerm("");
-                  setSearchItems([]);
-                }}
-              >
-                <img
-                  src={item.attributes.img.data.attributes.url}
-                  alt={item.attributes.title}
-                />
-                <div className="content">
-                  <h3>{item.attributes.title}</h3>
-                  <span>$ {item.attributes.price}</span>
-                </div>
-              </Link>
-            </div>
-          ))}
+          {searchItems?.map((item) => {
+            return (
+              <div className="searchItem" key={item.id}>
+                <Link
+                  className="searchLink"
+                  to={`/product/${item.attributes.title}`}
+                  onClick={() => {
+                    setSearchTerm("");
+                    setSearchItems([]);
+                  }}
+                >
+                  <img
+                    src={item.attributes.img.data.attributes.url}
+                    alt={item.attributes.title}
+                  />
+                  <div className="content">
+                    <h3>{item.attributes.title}</h3>
+                    <span>$ {item.attributes.price}</span>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
         </div>
-      )}
+      ) : null}
     </div>
   );
 }
