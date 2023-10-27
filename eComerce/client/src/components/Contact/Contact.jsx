3e52e35bf9fc933be8ddc57b22d5cbb39538b490
Gyunay Kadirov <gyunay@hotmail.dk>
2023-10-27 12:44:28 -0700

    fix: SARAFRI

--- eComerce/client/src/components/Contact/Contact.jsx
+++ eComerce/client/src/components/Contact/Contact.jsx
@@ -1,9 +1,9 @@
 import "./Contact.scss";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
-import TwitterIcon from "@mui/icons-material/Twitter";
 import GoogleIcon from "@mui/icons-material/Google";
 import PinterestIcon from "@mui/icons-material/Pinterest";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Contact = () => {
   return (
@@ -17,7 +17,7 @@ const Contact = () => {
         <div className="icons">
           <FacebookIcon className="icon" />
           <InstagramIcon className="icon" />
-          <TwitterIcon className="icon" />
+          <FaXTwitter className="icon" />
           <GoogleIcon className="icon" />
           <PinterestIcon className="icon" />
         </div>
