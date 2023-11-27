c967ab8bed3adae718eff3110559641f9a0554a7
OdinOfRivia <gyunay@hotmail.dk>
2023-11-26 17:20:47 -0800

    feat: add skeleton loading screen for main image in producs page

---
+++ eCommerce/client/src/components/Skeleton/LargeImageSkeleton/LargeImageSkeleton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LargeImageSkeleton.scss";
+
+const LargeImageSkeleton = () => {
+  return (
+    <div className="large-image-skeleton">
+      <div className="skeleton-large-image"></div>
+    </div>
+  );
+};
+
+export default LargeImageSkeleton;
