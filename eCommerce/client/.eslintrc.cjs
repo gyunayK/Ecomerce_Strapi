e354c31fc810b8aa80c3f592f7d4526ff7c2c949
Gyunay Kadirov <gyunay@hotmail.dk>
2024-09-18 17:20:56 -0700

    chore: remove prettier plugin

--- eCommerce/client/.eslintrc.cjs
+++ eCommerce/client/.eslintrc.cjs
@@ -6,13 +6,11 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    'prettier',
-    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', 'prettier'],
+  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
